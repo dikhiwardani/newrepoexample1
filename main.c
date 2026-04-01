3aa8428dc713fcef6c544bce597b0c7513cf3044
fadil-collab <fadilchitwo@gmail.com>
2026-04-01 21:48:08 +0700

    memnmbah kan fitur kamu

--- main.c
+++ main.c
@@ -14,6 +14,18 @@ void Dikhi(){
     printf("Dikhi Ganteng banget");
 }
 
+// ==========================================
+// ANGGOTA 3: KONVERSI BERAT
+// ==========================================
+void konversiBerat() {
+    float kg;
+    printf("\n--- Konversi Berat ---\n");
+    printf("Masukkan berat dalam Kilogram: ");
+    scanf("%f", &kg);
+    printf("Gram: %.2f g\n", kg * 1000);
+    printf("Pons (Lbs): %.2f lbs\n", kg * 2.20462);
+}
+
 int main(){
     for (int i = 0; i < 6; i++){
     printf("Dikhi ganteng");
