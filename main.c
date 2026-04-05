1e405ef3dbd9d730d0a30abc4843742b499ec325
fadil-collab <fadilchitwo@gmail.com>
2026-04-05 17:58:13 +0700

    asdasd

---
+++ main.c
@@ -0,0 +1,8 @@
+void konversiJarak() {
+    float km;
+    printf("\n--- Konversi Jarak ---\n");
+    printf("Masukkan jarak dalam Kilometer: ");
+    scanf("%f", &km);
+    printf("Meter: %.2f m\n", km * 1000);
+    printf("Centimeter: %.2f cm\n", km * 100000);
+} // <--- Tadi kamu lupa tutup kurung ini!
